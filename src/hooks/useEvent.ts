9f7edcec4227e5db0843e4206372e5d6343067d2
Robert Herber <robert@kingstinct.com>
2022-08-10 10:54:52 +0200

    Trying to improve useEvent typing

--- src/hooks/useEvent.ts
+++ src/hooks/useEvent.ts
@@ -2,10 +2,8 @@ import { useCallback, useEffect, useRef } from 'react'
 
 // https://gist.github.com/diegohaz/695097a06f038a707c3a1b11e4e40195
 
-type AnyFunction = (...args: readonly unknown[]) => void
-
-function useEvent<T extends AnyFunction>(handler: T) {
-  const handlerRef = useRef<AnyFunction>()
+function useEvent<T extends readonly unknown[], TRet extends Exclude<unknown, JSX.Element>>(handler: (...args: T) => TRet) {
+  const handlerRef = useRef<(...args: T) => TRet>()
 
   // In a real implementation, this would run before layout effects
   useEffect(() => {
@@ -14,7 +12,7 @@ function useEvent<T extends AnyFunction>(handler: T) {
 
   // eslint-disable-next-line
   // @ts-ignore
-  return useCallback<AnyFunction>((...args) => handlerRef.current?.apply(null, args), []) as T
+  return useCallback<(...args: T) => TRet>((...args) => handlerRef.current?.apply(null, args), [])
 }
 
 export default useEvent
