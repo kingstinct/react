4eaf2a6f4a51ef141205aef329c2ad7a2663785d
Robert Herber <robert@kingstinct.com>
2022-09-14 10:10:25 +0200

    feat: add status to AuthProvider

--- src/contexts/Auth.tsx
+++ src/contexts/Auth.tsx
@@ -1,15 +1,22 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import React, {
-  useEffect, useMemo, useState, createContext,
+  useEffect, useMemo, useState, createContext, useContext,
 } from 'react'
 
 import type { PropsWithChildren } from 'react'
+
+export enum Status {
+  INITIALIZING,
+  READY_WITH_TOKEN,
+  READY_WITHOUT_TOKEN,
+}
 
 type AuthContextData = {
   readonly token: string | null,
   readonly hasToken: boolean,
   readonly clearToken: () => void,
   readonly setToken: (token: string) => void
+  readonly status: Status
 }
 
 export const AuthContext = createContext<AuthContextData>({
@@ -17,40 +24,68 @@ export const AuthContext = createContext<AuthContextData>({
   hasToken: false,
   setToken: () => { },
   token: null,
+  status: Status.INITIALIZING,
 })
 
 const AUTH_TOKEN_KEY = 'AUTH_TOKEN'
 
+type AuthStateInternal = { readonly token: string | null, readonly isReady: boolean }
+
 const AuthProvider: React.FC<PropsWithChildren> = ({ children }) => {
-  const [token, setToken] = useState<string | null>(null)
+  const [{ token, isReady }, setToken] = useState<AuthStateInternal>({ token: null, isReady: false })
 
   useEffect(() => {
     const init = async () => {
       const t = await AsyncStorage.getItem(AUTH_TOKEN_KEY)
 
-      setToken(t)
+      setToken({ token: t, isReady: true })
     }
     void init()
   }, [])
 
   const value = useMemo<AuthContextData>(() => ({
     clearToken: () => {
-      setToken(null)
+      setToken({ token: null, isReady: true })
       void AsyncStorage.removeItem(AUTH_TOKEN_KEY)
     },
     hasToken: !!token,
     setToken: (t: string) => {
-      setToken(t)
+      setToken({ token: t, isReady: true })
       void AsyncStorage.setItem(AUTH_TOKEN_KEY, t)
     },
     token,
-  }), [token])
+    status: isReady ? (token ? Status.READY_WITH_TOKEN : Status.READY_WITHOUT_TOKEN) : Status.INITIALIZING,
+  }), [token, isReady])
 
   return (
     <AuthContext.Provider value={value}>
       {children}
     </AuthContext.Provider>
   )
+}
+
+export const useToken = () => {
+  const { token } = useContext(AuthContext)
+
+  return token
+}
+
+export const useStatus = () => {
+  const { status } = useContext(AuthContext)
+
+  return status
+}
+
+export const useSetToken = () => {
+  const { setToken } = useContext(AuthContext)
+
+  return setToken
+}
+
+export const useClearToken = () => {
+  const { clearToken } = useContext(AuthContext)
+
+  return clearToken
 }
 
 export default AuthProvider
