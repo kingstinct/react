dbc2058baa69b7b17044d1d81d163b01a7832bc0
Robert Herber <robert@kingstinct.com>
2023-07-24 12:51:06 +0200

    chore: bump to latest, working well locally with yarn

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,11 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   maxWorkers: '50%',
-  preset: 'jest-expo',
+  preset: 'jest-expo/universal',
   // setupFilesAfterEnv: ['./jest.setup.ts'],
   rootDir: 'src',
+  // transformIgnorePatterns:
+  // ['node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@react-native/js-polyfills)'],
   /* transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@bugsnag|@react-native-community|localforage-expo-filesystem-driver|victory-.*|victory-shared-events|victory-area|victory-bar|victory-native|victory-core|@react-native-seoul|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|expo-modules-core|jest-runner|@react-native/normalize-color|native-base|@react-native/polyfills|@sentry/.*)',
   ], */
